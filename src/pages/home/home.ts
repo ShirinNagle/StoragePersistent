b8f857fe2a5261bfc5fe0a33cd6864f816da1b2b
Shirin <g00363458@gmit.ie>
2019-03-27 15:50:35 +0000

    Example of persistent storage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {Storage} from '@ionic/storage';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+  information:string;
+  constructor(public navCtrl: NavController,
+     private storage:Storage) {
 
   }
 
+  openStausPage(){
+    this.navCtrl.push('StatusPage');//lazy loading
+  }
+  ionViewWillEnter(){
+    this.storage.get("status")
+    .then((val)=>{
+      this.information=val;
+    }).catch((err)=>{
+      console.log(err);
+    });
+  }
 }
